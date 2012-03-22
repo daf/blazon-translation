5147eac4c8a8521a26b54230bc61922e6af39bdb
luke.torjussen <luke.torjussen@googlemail.com>
2012-03-22 16:45:23 +0000

    advanced charge tests added

--- BlazonGrammarTests/junit/blazon/server/grammar/GrammarAdvancedChargeTest.java
+++ BlazonGrammarTests/junit/blazon/server/grammar/GrammarAdvancedChargeTest.java
@@ -1,9 +1,14 @@
 package blazon.server.grammar;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import org.antlr.runtime.RecognitionException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import blazon.shared.shield.charges.AdvancedCharge;
 import blazon.shared.shield.tinctures.TinctureType;
@@ -13,33 +18,53 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+@RunWith(Parameterized.class)
 public class GrammarAdvancedChargeTest {
-
-	@Test
-	public void testThatLionRampantOrCreatedCorrectObject() throws RecognitionException {
-		BlazonParser parser = new ParserCreator().createParser("lion rampant or");
-		Tinctures tinctures = new Tinctures();
-		List<AdvancedCharge> advancedCharges = parser.advanced_charge(tinctures, TinctureType.COLOUR, 1);
-		assertThat(advancedCharges.size(), is(1));
-		AdvancedCharge advCharge = advancedCharges.get(0);
-		assertThat(advCharge.getTextDescription(), is(equalTo("'lion rampant or'")));
+	
+	private String blazon;
+	private String answer;
+	private int number;
+	private TinctureType underTinctureType;
+	
+	public GrammarAdvancedChargeTest(String blazon, int number, TinctureType underTinctureType) {
+		this.blazon = blazon;
+		this.answer = "'" + blazon.replace(" and ", " ") + "'";
+		this.number = number;
+		this.underTinctureType = underTinctureType;
 	}
 	
-	
-	//beast with attitude
-	//with and without att Modifier
-	//winged beast w/ attitude
-	//with and without att Modifier
-	//winged beast with flying attitude
-	//with and without att Modifier
-	//bird flying insect with flying attitude
-	//with and without att Modifier
-	//swimming beast with swimming attitude
-	//with and without att Modifier
-	//beast on own
-	//attitude on own
-	// try one or 2 with plurals
-	// try one or 2 with body parts
-	// try one or 2 with rule of tincture
-	
+	@Parameters
+	public static Collection<Object[]> getBlazonSentences() {
+		return Arrays.asList(new Object[][] {
+			{ "lion rampant or", 1, TinctureType.COLOUR }, 
+			{ "lion rampant or langued gules", 1, TinctureType.COLOUR }, 
+			{ "lion rampant or langued gules and armed argent", 1, TinctureType.COLOUR }, 
+			{ "lions rampant or", 2, TinctureType.COLOUR }, 
+			{ "lion rampant guardant or", 1, TinctureType.COLOUR },
+			{ "dragon passant gules", 1, TinctureType.METAL},
+			{ "dragon passant reguardant gules", 1, TinctureType.METAL },
+			{ "dragon rising gules", 1, TinctureType.METAL },
+			{ "dragons rising gules", 3, TinctureType.METAL },
+			{ "dragon rising reguardant gules", 1, TinctureType.METAL },
+			{ "bee displayed gules", 1, TinctureType.METAL },
+			{ "bee displayed guardant gules", 1, TinctureType.COLOUR },
+			{ "fish naiant proper", 1, TinctureType.OTHER },
+			{ "fish naiant guardant proper", 1, TinctureType.OTHER },
+		});
+	}
+
+	@Test
+	public void testThatAdvancedChargeGetsReturnedCorrectly() throws RecognitionException {
+		BlazonParser parser = new ParserCreator().createParser(blazon);
+		Tinctures tinctures = new Tinctures();
+		List<AdvancedCharge> advancedCharges = parser.advanced_charge(tinctures, underTinctureType, number);
+		assertThat(advancedCharges.size(), is(number));
+		for(int i = 0; i < number; i++) {
+			AdvancedCharge advCharge = advancedCharges.get(i);
+			if (answer.contains(advCharge.getName() + "s")) {
+				answer = answer.replace(advCharge.getName() + "s", advCharge.getName());
+			}
+			assertThat(advCharge.getTextDescription(), is(equalTo(answer)));
+		}
+	}
 }
